bef787edc04254d0b913f7b0c6f428436418c600
unknown <119576534@qq.com>
2018-08-10 17:00:01 +0800

    modify SingletoTemplate

--- Common/SingltonTemplate.h
+++ Common/SingltonTemplate.h
@@ -5,10 +5,12 @@
 
 namespace KETTLE
 {
+    // 饿汉模式，需要做线程安全处理，双检测lock机制不是安全的，需要用一种方式处理
 	template<typename T>
 	class Singleton
 	{
 	public:
+        // DCLP(double check lock pattern) is not safe
 		static T*    GetInstance()
 		{
 			if (!__instance)
@@ -27,12 +29,31 @@ namespace KETTLE
 		}
 	protected:
 		Singleton() {};
-
-		static T*   __instance;
+        static T*   __instance;
 	};
 
 	template<typename T>
 	T* Singleton<T>::__instance = NULL;
+
+    // 懒汉模式，不做线程安全处理
+    template<typename T>
+    class Singleton2
+    {
+    public:
+        static T* GetInstance()
+        {
+            return __instance;
+        }
+
+    protected:
+        Singleton2() {}
+        virtual ~Singleton2() { SAFE_DELETE_PTR(__instance); }
+    private:
+        static T* __instance;
+    };
+
+    template<typename T>
+    T* Singleton2<T>::__instance = new T;
 }
 
 #endif
