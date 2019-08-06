0be5a4fba0a73ff98e5205aa5adb4a502c3444e8
inlcude <119576534@qq.com>
2019-08-06 17:49:42 +0800

    modify KETTLE

---
+++ Common/TcpClient.h
@@ -0,0 +1,15 @@
+#ifndef _TCP_CLIENT_H_
+#define _TCP_CLIENT_H_
+#include "KETTLEPlatform.h"
+
+using namespace KETTLE;
+namespace KETTLE{
+    class TcpClient{
+        public:
+            TcpClient();
+            ~TcpClient();
+            
+
+    };
+}
+#endif
